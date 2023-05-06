a36b816036b3b4e7045688a6f346240f301995e0
qarnet <qarnet>
2023-05-06 13:43:57 +0200

    Saved state

--- src/main.c
+++ src/main.c
@@ -12,11 +12,15 @@
 #include <zephyr/pm/device.h>
 #include <zephyr/pm/policy.h>
 #include <soc.h>
-#include "retained.h"
 #include <hal/nrf_gpio.h>
+#include <zephyr/drivers/gpio.h>
+#include <zephyr/drivers/spi.h>
+
+#include "sensors.h"
 
 #define BUSY_WAIT_S 2U
 #define SLEEP_S 2U
+static const struct gpio_dt_spec led_red = GPIO_DT_SPEC_GET(DT_ALIAS(led0), gpios);
 
 /* Prevent deep sleep (system off) from being entered on long timeouts
  * or `K_FOREVER` due to the default residency policy.
@@ -38,74 +42,79 @@ SYS_INIT(disable_ds_1, PRE_KERNEL_2, 0);
 void main(void)
 {
 	int rc;
-	const struct device *const cons = DEVICE_DT_GET(DT_CHOSEN(zephyr_console));
 
-	if (!device_is_ready(cons)) {
-		printk("%s: device not ready.\n", cons->name);
-		return;
+	uint8_t my_buffer = 0xB9;
+	struct spi_buf my_spi_buffer[1];
+	my_spi_buffer[0].buf = &my_buffer;
+	my_spi_buffer[0].len = 1;
+	const struct spi_buf_set tx_buff = {my_spi_buffer, 1};
+
+	const struct spi_dt_spec spi = SPI_DT_SPEC_GET(DT_ALIAS(spi_flash0), SPI_WORD_SET(8), 0);
+
+	if(!spi_is_ready_dt(&spi))
+	{
+		printk("SPI not ready\n");
 	}
 
-	printk("\n%s system off demo\n", CONFIG_BOARD);
+	// rc = spi_write_dt(&spi, &tx_buff);
+	// if(rc < 0)
+	// {
+	// 	printk("SPI Write failed (err %d)\n", rc);
+	// }
 
-	if (IS_ENABLED(CONFIG_APP_RETENTION)) {
-		bool retained_ok = retained_validate();
+	// const struct device *flash_dev = DEVICE_DT_GET(DT_ALIAS(spi_flash0));
 
-		/* Increment for this boot attempt and update. */
-		retained.boots += 1;
-		retained_update();
+	// if(!device_is_ready(flash_dev))
+	// {
+	// 	printk("Flash dev not ready\n");
+	// }
 
-		printk("Retained data: %s\n", retained_ok ? "valid" : "INVALID");
-		printk("Boot count: %u\n", retained.boots);
-		printk("Off count: %u\n", retained.off_count);
-		printk("Active Ticks: %" PRIu64 "\n", retained.uptime_sum);
-	} else {
-		printk("Retained data not supported\n");
-	}
+	// rc = pm_device_action_run(flash_dev, PM_DEVICE_ACTION_TURN_OFF);
+	// if(rc < 0)
+	// {
+	// 	printk("Error pm (err %d)\n", rc);
+	// }
 
-	/* Configure to generate PORT event (wakeup) on button 1 press. */
-	nrf_gpio_cfg_input(NRF_DT_GPIOS_TO_PSEL(DT_ALIAS(sw0), gpios),
-			   NRF_GPIO_PIN_PULLUP);
-	nrf_gpio_cfg_sense_set(NRF_DT_GPIOS_TO_PSEL(DT_ALIAS(sw0), gpios),
-			       NRF_GPIO_PIN_SENSE_LOW);
 
-	printk("Busy-wait %u s\n", BUSY_WAIT_S);
-	k_busy_wait(BUSY_WAIT_S * USEC_PER_SEC);
+	// initSensors();
 
-	printk("Busy-wait %u s with UART off\n", BUSY_WAIT_S);
-	rc = pm_device_action_run(cons, PM_DEVICE_ACTION_SUSPEND);
-	k_busy_wait(BUSY_WAIT_S * USEC_PER_SEC);
-	rc = pm_device_action_run(cons, PM_DEVICE_ACTION_RESUME);
+	gpio_pin_configure_dt(&led_red, GPIO_OUTPUT_INACTIVE);
 
-	printk("Sleep %u s\n", SLEEP_S);
-	k_sleep(K_SECONDS(SLEEP_S));
+	printk("\n%s system off demo\n", __TIME__);
 
-	printk("Sleep %u s with UART off\n", SLEEP_S);
-	rc = pm_device_action_run(cons, PM_DEVICE_ACTION_SUSPEND);
-	k_sleep(K_SECONDS(SLEEP_S));
-	rc = pm_device_action_run(cons, PM_DEVICE_ACTION_RESUME);
+	// k_sleep(K_FOREVER);
 
-	printk("Entering system off; press BUTTON1 to restart\n");
+	gpio_pin_set_dt(&led_red, 1);
 
-	if (IS_ENABLED(CONFIG_APP_RETENTION)) {
-		/* Update the retained state */
-		retained.off_count += 1;
-		retained_update();
-	}
+	k_msleep(1000);
+
+	gpio_pin_set_dt(&led_red, 0);
+
+	k_msleep(1000);
+
+	NRF_CLOCK->TASKS_HFCLKSTOP;
+	NRF_RTC0->TASKS_STOP;
+	NRF_RTC1->TASKS_STOP;
+	NRF_RTC2->TASKS_STOP;
 
 	/* Above we disabled entry to deep sleep based on duration of
 	 * controlled delay.  Here we need to override that, then
 	 * force entry to deep sleep on any delay.
 	 */
-	pm_state_force(0u, &(struct pm_state_info){PM_STATE_SOFT_OFF, 0, 0});
+	// pm_state_force(0u, &(struct pm_state_info){PM_STATE_SOFT_OFF, 0, 0});
 
 	/* Now we need to go sleep. This will let the idle thread runs and
 	 * the pm subsystem will use the forced state. To confirm that the
 	 * forced state is used, lets set the same timeout used previously.
-	 */
+	 */	
 	k_sleep(K_SECONDS(SLEEP_S));
+
+	// gpio_pin_set_dt(&led_red, 0);
 
 	printk("ERROR: System off failed\n");
 	while (true) {
 		/* spin to avoid fall-off behavior */
 	}
+
+	
 }
